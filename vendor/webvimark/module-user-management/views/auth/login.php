640de985d31432163928abd4fe0d7cba52898e37
anasshafqat <anasshafqat01@gmail.com>
2020-03-26 14:51:42 +0500

    vendor updated

---
+++ vendor/webvimark/module-user-management/views/auth/login.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * @var $this yii\web\View
+ * @var $model webvimark\modules\UserManagement\models\forms\LoginForm
+ */
+
+use webvimark\modules\UserManagement\components\GhostHtml;
+use webvimark\modules\UserManagement\UserManagementModule;
+use yii\bootstrap\ActiveForm;
+use yii\helpers\Html;
+?>
+
+<div class="container" id="login-wrapper">
+	<div class="row">
+		<div class="col-md-4 col-md-offset-4">
+			<div class="panel panel-default" style="border: 3px solid #0050AD;">
+				<div class="panel-heading">
+					<img src="images/login.png" class="img-responsive">
+					<!-- <h3 class="panel-title"><?= UserManagementModule::t('front', 'Authorization') ?></h3> -->
+				</div>
+				<div class="panel-body">
+
+					<?php $form = ActiveForm::begin([
+						'id'      => 'login-form',
+						'options'=>['autocomplete'=>'off'],
+						'validateOnBlur'=>false,
+						'fieldConfig' => [
+							'template'=>"{input}\n{error}",
+						],
+					]) ?>
+
+					<?= $form->field($model, 'username')
+						->textInput(['placeholder'=>$model->getAttributeLabel('username'), 'autocomplete'=>'off']) ?>
+
+					<?= $form->field($model, 'password')
+						->passwordInput(['placeholder'=>$model->getAttributeLabel('password'), 'autocomplete'=>'off']) ?>
+
+					<?= (isset(Yii::$app->user->enableAutoLogin) && Yii::$app->user->enableAutoLogin) ? $form->field($model, 'rememberMe')->checkbox(['value'=>true]) : '' ?>
+
+					<?= Html::submitButton(
+						UserManagementModule::t('front', 'Login'),
+						['class' => 'btn btn-primary btn-flat btn-block']
+					) ?>
+
+					<div class="row registration-block">
+						<div class="col-sm-6">
+							<?= GhostHtml::a(
+								UserManagementModule::t('front', "Registration"),
+								['/user-management/auth/registration']
+							) ?>
+						</div>
+						<div class="col-sm-6 text-right">
+							<?= GhostHtml::a(
+								UserManagementModule::t('front', "Forgot password ?"),
+								['/user-management/auth/password-recovery']
+							) ?>
+						</div>
+					</div>
+					<?php ActiveForm::end() ?>
+					<table class="table table-hover table-striped table-condensed table-bordered">
+						<tr>
+							<th colspan="2" class="text-center bg-info" style="color: #286090">Login Credentials</th>
+						</tr>
+						<tr>
+							<th>Username</th>
+							<th>Password</th>
+						</tr>
+						<tr>
+							<td>Superadmin</td>
+							<td>superadmin</td>
+						</tr>
+						<tr>
+							<td>Admin</td>
+							<td>dexdevs321</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php
+$css = <<<CSS
+html, body {
+	background: #E8F0FE;
+	-webkit-box-shadow: inset 0 0 100px rgba(0,0,0,.5);
+	box-shadow: inset 0 0 100px rgba(0,0,0,.5);
+	height: 100%;
+	min-height: 100%;
+	position: relative;
+	border: 15px solid #000000	;
+	border-radius: 15px 30px;
+}
+#login-wrapper {
+	position: relative;
+	top: 6%;
+}
+#login-wrapper .registration-block {
+	margin-top: 15px;
+}
+CSS;
+
+$this->registerCss($css);
+?>
---
+++ vendor/yiisoft/extensions.php
@@ -0,0 +1,142 @@
+<?php
+
+$vendorDir = dirname(__DIR__);
+
+return array (
+  'yiisoft/yii2-bootstrap' => 
+  array (
+    'name' => 'yiisoft/yii2-bootstrap',
+    'version' => '2.0.10.0',
+    'alias' => 
+    array (
+      '@yii/bootstrap' => $vendorDir . '/yiisoft/yii2-bootstrap/src',
+    ),
+  ),
+  'yiisoft/yii2-debug' => 
+  array (
+    'name' => 'yiisoft/yii2-debug',
+    'version' => '2.1.13.0',
+    'alias' => 
+    array (
+      '@yii/debug' => $vendorDir . '/yiisoft/yii2-debug/src',
+    ),
+  ),
+  'yiisoft/yii2-gii' => 
+  array (
+    'name' => 'yiisoft/yii2-gii',
+    'version' => '2.1.4.0',
+    'alias' => 
+    array (
+      '@yii/gii' => $vendorDir . '/yiisoft/yii2-gii/src',
+    ),
+  ),
+  'yiisoft/yii2-faker' => 
+  array (
+    'name' => 'yiisoft/yii2-faker',
+    'version' => '2.0.4.0',
+    'alias' => 
+    array (
+      '@yii/faker' => $vendorDir . '/yiisoft/yii2-faker',
+    ),
+  ),
+  'yiisoft/yii2-swiftmailer' => 
+  array (
+    'name' => 'yiisoft/yii2-swiftmailer',
+    'version' => '2.1.2.0',
+    'alias' => 
+    array (
+      '@yii/swiftmailer' => $vendorDir . '/yiisoft/yii2-swiftmailer/src',
+    ),
+  ),
+  'ramosisw/yii2-material-dashboard' => 
+  array (
+    'name' => 'ramosisw/yii2-material-dashboard',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@ramosisw/CImaterial' => $vendorDir . '/ramosisw/yii2-material-dashboard',
+    ),
+  ),
+  'raoul2000/yii2-workflow' => 
+  array (
+    'name' => 'raoul2000/yii2-workflow',
+    'version' => '1.2.0.0',
+    'alias' => 
+    array (
+      '@raoul2000/workflow' => $vendorDir . '/raoul2000/yii2-workflow/src',
+    ),
+  ),
+  'raoul2000/yii2-workflow-view' => 
+  array (
+    'name' => 'raoul2000/yii2-workflow-view',
+    'version' => '0.0.2.0',
+    'alias' => 
+    array (
+      '@raoul2000/workflow/view' => $vendorDir . '/raoul2000/yii2-workflow-view/src',
+    ),
+  ),
+  'yiisoft/yii2-jui' => 
+  array (
+    'name' => 'yiisoft/yii2-jui',
+    'version' => '2.0.7.0',
+    'alias' => 
+    array (
+      '@yii/jui' => $vendorDir . '/yiisoft/yii2-jui',
+    ),
+  ),
+  'cornernote/yii2-workflow-manager' => 
+  array (
+    'name' => 'cornernote/yii2-workflow-manager',
+    'version' => '1.0.2.0',
+    'alias' => 
+    array (
+      '@cornernote/workflow/manager' => $vendorDir . '/cornernote/yii2-workflow-manager/src',
+    ),
+  ),
+  'yii2mod/yii2-enum' => 
+  array (
+    'name' => 'yii2mod/yii2-enum',
+    'version' => '1.7.1.0',
+    'alias' => 
+    array (
+      '@yii2mod/enum' => $vendorDir . '/yii2mod/yii2-enum',
+    ),
+  ),
+  'floor12/yii2-module-backup' => 
+  array (
+    'name' => 'floor12/yii2-module-backup',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@floor12/backup' => $vendorDir . '/floor12/yii2-module-backup/src',
+    ),
+  ),
+  '2amigos/yii2-chartjs-widget' => 
+  array (
+    'name' => '2amigos/yii2-chartjs-widget',
+    'version' => '2.1.3.0',
+    'alias' => 
+    array (
+      '@dosamigos/chartjs' => $vendorDir . '/2amigos/yii2-chartjs-widget/src',
+    ),
+  ),
+  'bedezign/yii2-audit' => 
+  array (
+    'name' => 'bedezign/yii2-audit',
+    'version' => '1.1.2.0',
+    'alias' => 
+    array (
+      '@bedezign/yii2/audit' => $vendorDir . '/bedezign/yii2-audit/src',
+    ),
+    'bootstrap' => 'bedezign\\yii2\\audit\\Bootstrap',
+  ),
+  'ruturajmaniyar/yii2-audit-log' => 
+  array (
+    'name' => 'ruturajmaniyar/yii2-audit-log',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@ruturajmaniyar/mod/audit' => $vendorDir . '/ruturajmaniyar/yii2-audit-log/src',
+    ),
+  ),
+);
